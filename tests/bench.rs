55597677f5afc138adf12f6f1c6de91c7990c13b
max <max@blue.tools>
2026-08-21 19:20:05 +0900

    Write versioned benchmark JSON

--- tests/bench.rs
+++ tests/bench.rs
@@ -8,11 +8,11 @@ mod common;
 #[ignore]
 fn bench() {
     let report = soksak_contract_terminal::bench::run::<common::Unit>("shitty");
-    println!("{}", report.to_line());
+    println!("{}", report.to_json());
     if let Ok(directory) = std::env::var("SOKSAK_BENCH_OUT") {
         let directory = std::path::PathBuf::from(directory);
         std::fs::create_dir_all(&directory).expect("create benchmark directory");
-        std::fs::write(directory.join("shitty.bench"), report.to_line())
+        std::fs::write(directory.join("shitty.bench.json"), report.to_json())
             .expect("write Shitty benchmark");
     }
     soksak_contract_terminal::bench::assert_within_budget(&report);
