ef5f2ef8ae6a32269379e9825168fb559492b6b8
max <max@blue.tools>
2026-08-23 20:16:09 +0900

    fix(release): package declared Shitty licenses

--- scripts/package-release.sh
+++ scripts/package-release.sh
@@ -10,14 +10,13 @@ cp sidecar.json "$package/sidecar.json"
 cp LICENSE LICENSE.GPL3 LICENSE.MIT "$package/"
 cp "$dist/soksak-sidecar-terminal-shitty" "$package/dist/"
 find "$package" -type l -exec false {} +
-tar -czf "$out" -C "$package" LICENSE THIRD-PARTY-NOTICES sidecar.json dist
-contents=$(tar -tzf "$out" | LC_ALL=C sort)
-expected='./
-./LICENSE
-./LICENSE.GPL3
-./LICENSE.MIT
-./dist/
-./dist/soksak-sidecar-terminal-shitty
-./sidecar.json'
+tar -czf "$out" -C "$package" LICENSE LICENSE.GPL3 LICENSE.MIT sidecar.json dist
+	contents=$(tar -tzf "$out" | LC_ALL=C sort)
+expected='LICENSE
+LICENSE.GPL3
+LICENSE.MIT
+dist/
+dist/soksak-sidecar-terminal-shitty
+sidecar.json'
 test "$contents" = "$expected"
 printf 'packaged %s for %s\n' "$out" "$target"
